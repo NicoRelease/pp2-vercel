320efa807d7b8f64181177b0c4c4bb14dc76a387
Nicolas Mafone <nicolasmafone@gmail.com>
2026-02-09 09:50:05 -0300

    cambios en backend para ajustar rutas en vercel

--- Backend/api/index.js
+++ Backend/api/index.js
@@ -1,13 +1,12 @@
 // api/index.js
 
-// Importa la aplicación Express definida en server.js
 import log from './log.js';
 import app from '../server.js'; 
 
 if (process.env.NODE_ENV !== 'production' && !process.env.VERCEL) {
-    // Si no estamos en producción Y no estamos en Vercel, iniciamos el listener local.
+
     app.listen(3000, () => {
-        //console.log('✅ Servidor Backend LOCAL escuchando en http://localhost:3000');
+    
     });
 }
 export default app; 
